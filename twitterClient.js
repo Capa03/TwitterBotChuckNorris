1285c57365bc2a4930162279d3c30a24c9acc5f0
David Capa <capaseitadavid@gmail.com>
2022-05-18 12:32:32 +0100

    ignore file

--- twitterClient.js
+++ twitterClient.js
@@ -1,12 +1,13 @@
 
 const{TwitterApi} = require("twitter-api-v2");
 
+require("dotenv").config();
 const client = new TwitterApi({
 
-    appKey: "TuZfLKk1B1n9309yWVwFOPRUc",
-    appSecret: "sVhapTUHBckbFtTdiYW5U1ogRY6cXMiN2aZWizx1WjKYzioH5E",
-    accessSecret: "y4kZadB5RhNTlJ6RFloWVMp5M43cVdIfl75de9Ksdrfsf",
-    accessToken: "873223092750995456-EHzInhs3ZWRhqpi8kcdn7QlWmG17yQC"
+    appKey: process.env.APP_KEY,
+    appSecret: process.env.APP_SECRET,
+    accessSecret: process.env.ACCESS_SECRET,
+    accessToken: process.env.ACCESS_TOKEN
 })
 
 
